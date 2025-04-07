e20a8d36f7b717e0f14b6398d7448389bfa5b7a8
AneesaAmeer <166473964+AneesaAmeer@users.noreply.github.com>
2025-04-07 19:24:25 +0530

    Update ProductPage.java

--- src/test/java/com/automationtask/pageObjects/ProductPage.java
+++ src/test/java/com/automationtask/pageObjects/ProductPage.java
@@ -15,7 +15,7 @@ public class ProductPage {
 
     public ProductPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30)); // Adjust wait time
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30)); 
     }
 
     public void verifyProductPage() {
